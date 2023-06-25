988dfa9badc3fe754f780a435b1f1f1f13b9b74a
bluesentinelsec <bluesentinel@protonmail.com>
2023-06-25 14:48:28 -0400

    setup package structure

---
+++ miley/client.py
@@ -0,0 +1,4 @@
+
+def query_hash():
+    print("query_hash function")
+    return 0
--- miley/main.py
+++ miley/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+from miley.client import query_hash
+
+
 def main():
     print("we're here!")
+    query_hash()
 
 if __name__ == "__main__":
     main()
